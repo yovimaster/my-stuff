a5c1c25226ad7fe5a07bad4c1533fc17d07415cd
Yoav Levi <master.tavas@gmail.com>
2019-05-29 20:44:30 +0300

    support URL as ARG

--- openuniversity.sh
+++ openuniversity.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+URL=$1
 BASENAME='media_b1800000'
-VAR='XnO8F9g91u'
-VIDEOPATH='https://sslstream.bynetcdn.com/vod/mp4:vod/openu/PRV1/'$VAR'/App/'$VAR'_6.mp4'
+token=(${URL//\?/ })
+VAR=(${URL//\// })
 
+VIDEOPATH='https://souvod.bynetcdn.com/vod/smil:vod/openu/'${VAR[5]}'/'${VAR[6]}'/App/'${VAR[6]}'_10.smil'
 
-for i in {1..1000}
+for i in {1..1200}
 do
-FILENAME=$BASENAME'_'$i'.ts'
+FILENAME=$BASENAME'_'$i'.ts?'${token[1]}
 FULLPATH=$VIDEOPATH/$FILENAME
 wget $FULLPATH
-cat $FILENAME >> 'fullvideo.ts'
+cat $FILENAME >> $2'.mp4'
 rm $FILENAME
 done
