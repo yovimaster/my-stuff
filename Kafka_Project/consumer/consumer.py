394221fa7992c39a4105a7c565e8a81a76041458
yovimaster <master.tavas@gmail.com>
2021-09-08 21:44:43 +0300

    typo

--- Kafka_Project/consumer/consumer.py
+++ Kafka_Project/consumer/consumer.py
@@ -4,15 +4,15 @@ import time
 from kafka import KafkaConsumer
 from prometheus_client import start_http_server, Summary, Counter
 
-messages_in = Counter('messeges_in', 'number of messages sent to kafka')
+messages_in = Counter('messages_in', 'number of messages sent to kafka')
 
 def main():
     try:
-        consumer =  KafkaConsumer(os.environ['TOPIC_NAME'], os.environ['BOOTSTRAP_SERVERS'])
+        consumer =  KafkaConsumer(os.environ['TOPIC_NAME'], bootstrap_servers=os.environ['BOOTSTRAP_SERVERS'])
     except:
         sys.stdout.write('Failed connecting to kafka\n')
         sys.exit(1)
-    
+
     for message in consumer:
         print("{}:{}:{}: key={} value={}".format(message.topic, message.partition, message.offset, message.key,message.value))
         messages_in.inc()
